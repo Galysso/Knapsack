5a0570e629c6002a71c91b8cd943d28a68a52a55
Valentin <v.antuori@gmail.com>
2017-06-09 14:14:24 +0200

    tas adapté aux noeuds d'un sac à dos bidimensionnel

--- main.c
+++ main.c
@@ -7,6 +7,9 @@
 int main() {
 	srand(5);
 
+	Tas *tas = malloc(sizeof(Tas));
+	TAS_initialiser(tas, 2);
+
 	probleme *p = genererProblemeTest();
 
 	for (int i = 0; i < p->n; ++i) {
@@ -17,41 +20,61 @@ int main() {
 	noeud ***noeuds = genererGraphe(p, &n);
 
 	afficherGraphe(noeuds[0][0], 0);
+	TAS_ajouter(tas, noeuds[0][0]);
 	printf("\n");
 
 	afficherGraphe(noeuds[1][0], 1);
+	TAS_ajouter(tas, noeuds[1][0]);
 	printf("\n");
 	afficherGraphe(noeuds[1][1], 1);
+	TAS_ajouter(tas, noeuds[1][1]);
 	printf("\n");
 
 	afficherGraphe(noeuds[2][0], 2);
+	TAS_ajouter(tas, noeuds[2][0]);
 	printf("\n");
 	afficherGraphe(noeuds[2][1], 2);
+	TAS_ajouter(tas, noeuds[2][1]);
 	printf("\n");
 	afficherGraphe(noeuds[2][2], 2);
+	TAS_ajouter(tas, noeuds[2][2]);
 	printf("\n");
 
 	afficherGraphe(noeuds[3][0], 3);
+	TAS_ajouter(tas, noeuds[3][0]);
 	printf("\n");
 	afficherGraphe(noeuds[3][1], 3);
+	TAS_ajouter(tas, noeuds[3][1]);
 	printf("\n");
 	afficherGraphe(noeuds[3][2], 3);
+	TAS_ajouter(tas, noeuds[3][2]);
 	printf("\n");
 	afficherGraphe(noeuds[3][3], 3);
+	TAS_ajouter(tas, noeuds[3][3]);
 	printf("\n");
 
 	afficherGraphe(noeuds[4][0], 4);
+	TAS_ajouter(tas, noeuds[4][0]);
 	printf("\n");
 	afficherGraphe(noeuds[4][1], 4);
+	TAS_ajouter(tas, noeuds[4][1]);
 	printf("\n");
 	afficherGraphe(noeuds[4][2], 4);
+	TAS_ajouter(tas, noeuds[4][2]);
 	printf("\n");
 	afficherGraphe(noeuds[4][3], 4);
+	TAS_ajouter(tas, noeuds[4][3]);
 	printf("\n");
 	afficherGraphe(noeuds[4][4], 4);
+	TAS_ajouter(tas, noeuds[4][4]);
 	printf("\n");
 	afficherGraphe(noeuds[4][5], 4);
+	TAS_ajouter(tas, noeuds[4][5]);
 	printf("\n");
+
+	TAS_retirerInd(tas, 1);
+
+	TAS_afficher(tas);
 
 	printf("COCO\n");
 }
--- src/tas.c
+++ src/tas.c
@@ -6,25 +6,25 @@
 
 
 void TAS_initialiser(Tas *tas, unsigned int taille) {
-	tas->tab = malloc(taille*sizeof(int));
+	tas->tab = malloc(taille*sizeof(noeud *));
 	tas->taille = taille;
 	tas->n = 0;
 }
 
-void TAS_ajouter(Tas *tas, int val) {
+void TAS_ajouter(Tas *tas, noeud *val) {
 	if (tas->n == tas->taille) {
 		tas->taille = 2*tas->taille;
-		tas->tab = realloc(tas->tab, tas->taille*sizeof(int));
+		tas->tab = realloc(tas->tab, tas->taille*sizeof(noeud *));
 	}
 	unsigned int indP;	// l'indice du père
 	unsigned int indC;	// l'indice de parcours
-	int tmp;			// valeur temporaire pour échange
-	int *tab = tas->tab;
+	noeud *tmp;			// valeur temporaire pour échange
+	noeud **tab = tas->tab;
 	tab[tas->n] = val;	// on insère la valeur à la fin du tableau
 	indC = tas->n;
-	indP = (indC-1)/2;		// on récupère l'indice du père (= indC si c'est la racine)
+	indP = (indC-1)/2;	// on récupère l'indice du père (= indC si c'est la racine)
 
-	while ((indC > 0) && (tab[indC] > tab[indP])) {	// tant que son père est plus petit
+	while ((indC > 0) && (tab[indC]->val > tab[indP]->val)) {	// tant que son père est plus petit
 		tmp = tab[indP];				// on échange le noeud avec son père
 		tab[indP] = tab[indC];
 		tab[indC] = tmp;
@@ -42,11 +42,11 @@ void TAS_retirerMax(Tas *tas) {
 void TAS_retirerInd(Tas *tas, int ind) {
 	unsigned int indFG;		// l'indice du fils gauche
 	unsigned int indFD;		// l'indice du fils droit
-	int tmp;				// valeur temporaire pour échange
+	noeud *tmp;				// valeur temporaire pour échange
 	bool fini = false;
 	tas->n = tas->n - 1;	// il y a un élément de moins dans le tas
 	int n = tas->n;
-	int *tab = tas->tab;
+	noeud **tab = tas->tab;
 	tab[ind] = tas->tab[n];	// on remplace l'élément à supprimer par le dernier du tas	
 
 	do {//TAS_afficher(tas);
@@ -54,8 +54,8 @@ void TAS_retirerInd(Tas *tas, int ind) {
 		indFD = 2*ind + 2;
 		//printf("ind= %d indFG=%d indFD=%d\n", ind, indFG, indFD);
 		if (indFD < n) {
-			if (tab[indFD] > tab[indFG]) {
-				if (tab[indFD] > tab[ind]) {
+			if (tab[indFD]->val > tab[indFG]->val) {
+				if (tab[indFD]->val > tab[ind]->val) {
 					tmp = tab[indFD];
 					tab[indFD] = tab[ind];
 					tab[ind] = tmp;
@@ -64,7 +64,7 @@ void TAS_retirerInd(Tas *tas, int ind) {
 					fini = true;
 				}
 			} else {
-				if (tab[indFG] > tab[ind]) {
+				if (tab[indFG]->val > tab[ind]->val) {
 					tmp = tab[indFG];
 					tab[indFG] = tab[ind];
 					tab[ind] = tmp;
@@ -73,7 +73,7 @@ void TAS_retirerInd(Tas *tas, int ind) {
 					fini = true;
 				}
 			}
-		} else if ((indFG < n) && (tab[indFG] > tab[ind])) {
+		} else if ((indFG < n) && (tab[indFG]->val > tab[ind]->val)) {
 			tmp = tab[indFG];
 			tab[indFG] = tab[ind];
 			tab[ind] = tmp;
@@ -93,11 +93,20 @@ void TAS_maximum(Tas *tas) {
 }
 
 void TAS_afficher(Tas *tas) {
-	int *tab = tas->tab;
+	noeud **tab = tas->tab;
 	int n = tas->n;
+	int sautLigne = 1;
+	int curseur = 0;
 	printf("n=%d \n|", n);
 	for (int i = 0; i < n; ++i) {
-		printf("%d|", tab[i]);
+		if (curseur == sautLigne) {
+			printf("\n|%d|", tab[i]->val);
+			sautLigne = 2*sautLigne;
+			curseur = 1;
+		} else {
+			printf("%d|", tab[i]->val);
+			++curseur;
+		}
 	}
 	printf("\n");
 }
